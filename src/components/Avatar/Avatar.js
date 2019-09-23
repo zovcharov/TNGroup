67e34940b1aa92ec43a2ee7bb39bf99cb205e2ca
Konstantin Kirilin <konstantin@paulcamper.de>
2019-09-23 22:45:26 +0300

    TNG-6 fixed bug with avatar sizes and added l size

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -6,7 +6,7 @@ const AVAILABLE_SIZES = ['s', 'm', 'l'];
 const DEFAULT_SIZE = 'm';
 
 const getClassForSize = (size) => {
-    return AVAILABLE_SIZES.indexOf(size) === -1 ? `avatar_size_${size}` : `avatar_size_${DEFAULT_SIZE}`
+    return AVAILABLE_SIZES.indexOf(size) !== -1 ? `avatar_size_${size}` : `avatar_size_${DEFAULT_SIZE}`
 };
 
 const Avatar = (props) => {
