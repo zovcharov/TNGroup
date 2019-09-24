e62d259de3dec2cad355f8464cd96ad1a606cac9
Konstantin Kirilin <konstantin@paulcamper.de>
2019-09-24 23:06:09 +0300

    TNG-6 added profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Avatar from '../Avatar/Avatar'
+import './Profile.scss'
+import { Dropdown, DropdownItem } from '../Dropdown/Dropdown'
+
+const Profile = (props) => {
+  const {
+    avatar = 'https://www.rusdialog.ru/images/news/news_view/5107a4952d8cf8344551a6c9f4f80d0b.jpg',
+    name = 'Абыхуйлов Хуйтимберг Хуепуталов'
+  } = props
+  return (
+    <div className='profile'>
+      <Avatar size='l' avatarUrl={avatar} />
+      <Dropdown
+        label={name}
+        labelClass='profile__name'
+        menuClass='profile__menu'
+        buttonClass='profile__button'>
+          <DropdownItem>qweqwe</DropdownItem>
+          <DropdownItem>qweqwe</DropdownItem>
+        </Dropdown>
+    </div>
+  )
+}
+
+export default Profile
