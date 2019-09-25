92b6d4518324ca1efc173d697c56126fd7e42662
Konstantin Kirilin <konstantin@paulcamper.de>
2019-09-25 21:43:58 +0300

    TNG-6 added top nav to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,13 @@ import React from 'react'
 import './Header.scss'
 import Logo from '../Logo/Logo'
 import Profile from '../Profile/Profile'
+import TopNav from '../TopNav/TopNav'
 
 const Header = () => {
   return (
     <header className='header'>
       <Logo />
+      <TopNav />
       <Profile />
     </header>
   )
