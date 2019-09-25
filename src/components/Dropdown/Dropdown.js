117b148cb3611967c3f8254efccc1909d4896e50
Konstantin Kirilin <konstantin@paulcamper.de>
2019-09-25 22:05:57 +0300

    TNG-6 optimised effect in dropdown

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -43,7 +43,7 @@ const Dropdown = (props) => {
       document.removeEventListener('click', handleClickOutside, false)
       document.removeEventListener('contextmenu', handleClickOutside, false)
     }
-  })
+  }, [])
 
   return (
     <div
