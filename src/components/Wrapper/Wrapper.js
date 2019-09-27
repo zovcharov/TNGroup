8e4280b5d09105062728568f6d85683799273c44
ggnorton <32387814+ggnorton@users.noreply.github.com>
2019-09-27 20:27:41 +0300

    Tng 5 Added sidebar (#6)
    * TNG-5 added OpenSans font
    
    * TNG-5 added main css
    
    * TNG-5 added fonts load
    
    * TNG-5 fixed paddings in header
    
    * TNG-5 added sideMenu and menu items
    
    * TNG-5 added sidebar component
    
    * TNG-5 added sidebar in wrapper and tweaked styles

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './Wrapper.scss'
 import Header from '../Header/Header'
+import Sidebar from '../Sidebar/Sidebar'
 
 const Wrapper = ({children}) => {
   return (
     <div className='wrapper'>
       <Header />
+      <Sidebar />
       <div className='content'>
         {children}
       </div>
