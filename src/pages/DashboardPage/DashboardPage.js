1a7b742520f3cc14f6663f21aad8b4208b6b6cee
Konstantin Kirilin <konstantin@paulcamper.de>
2019-09-27 22:05:32 +0300

    TNG-9 added dashboard page component

---
+++ src/pages/DashboardPage/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './DashboardPage.scss'
+
+const DashboardPage = (props) => {
+  return (
+    <div className='dashboard-page'>
+
+    </div>
+  )
+}
+
+export default DashboardPage
