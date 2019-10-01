b9a81e060b5d57a4d3c89626436969d7cc0de18b
ggnorton <32387814+ggnorton@users.noreply.github.com>
2019-10-01 22:23:40 +0300

    Tng 13 (#12)
    * TNG-13 added nav search component
    
    * TNG-13 fixed top nav width
    
    * TNG-13 added nav search component
    
    * TNG-13 tweaked profile component paddings
    
    * TNG-13 tweaked top nav width
    
    * TNG-13 finished for now with nav search
    
    * TNG-13 fixed width of profile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,13 @@ import './Header.scss'
 import Logo from '../Logo/Logo'
 import Profile from '../Profile/Profile'
 import TopNav from '../TopNav/TopNav'
+import NavSearch from '../NavSearch/NavSearch'
 
 const Header = () => {
   return (
     <header className='header'>
       <Logo />
+      <NavSearch />
       <TopNav />
       <Profile />
     </header>
---
+++ src/components/NavSearch/NavSearch.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import './NavSearch.scss'
+
+const NavSearch = (props) => {
+  const {
+    onSubmit = () => {}
+  } = props
+
+  const [value, changeValue] = useState('')
+
+  const submit = () => {
+    if (value !== '') {
+      onSubmit(value)
+    }
+  }
+
+  const onPressEnter = (event) => {
+    if (event.key === 'Enter') {
+      submit()
+    }
+  }
+
+  return (
+    <div className='nav-search'>
+      <input
+        value={value}
+        onChange={event => changeValue(event.target.value)}
+        spellcheck="false"
+        type='text'
+        onKeyPress={onPressEnter}
+        className='nav-search__input' />
+      <span className='nav-search__icon' onClick={submit} />
+    </div>
+  )
+}
+
+NavSearch.propTypes = {
+  onSubmit: PropTypes.func
+}
+
+export default NavSearch
