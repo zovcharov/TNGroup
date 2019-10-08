467d5a605e58be50e39b223ff8159f3183cdfab1
Konstantin Kirilin <konstantin@paulcamper.de>
2019-10-08 22:07:26 +0300

    TNG-16 added styles and refactored table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Table.scss'
 
 const Table = (props) => {
   const {
@@ -8,36 +9,79 @@ const Table = (props) => {
   } = props
   return (
     <div className='table'>
-      <div className='table__header'>
-        {columns.map((column, index) => {
-          return (
-            <div
-              className='table__header-cell'
-              key={index}>
-              {column.label}
-            </div>
-          )
-        })}
-      </div>
-      <div className='table__body'>
+      <TableHead columns={columns} />
+      <TableBody items={items} columns={columns} />
+    </div>
+  )
+}
+
+const TableCell = (props) => {
+  const {
+    children,
+    width
+  } = props
+  return (
+    <div
+      className='table__cell'
+      style={{width: width}}>
+      {children}
+    </div>
+  )
+}
+
+const TableBody = (props) => {
+  const {
+    items = [],
+    columns = []
+  } = props
+  return (
+    <div className='table__body'>
         {items.map((item, index) => {
           return (
             <div
               className='table__row'
               key={index}>
-                {columns.map(column => {
+                {columns.map(({name, width}, index) => {
                   return (
-                    <div className='table__cell'>
-                      {item[column.name]}
-                    </div>
+                    <TableCell key={index} width={width}>
+                      {item[name]}
+                    </TableCell>
                   )
                 })}
             </div>
           )
         })}
       </div>
+  )
+}
+
+const TableHead = (props) => {
+  const {
+    columns = []
+  } = props
+  return (
+    <div className='table__header'>
+      <div className='table__row'>
+        {columns.map(({label = '', width='auto' }, index) => {
+          return (
+            <div
+              className='table__header-cell'
+              key={index}
+              style={{
+                width: width
+              }}>
+              {label}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
+}
+
+Table.propTypes = {
+  columns: PropTypes.array.isRequired,
+  items: PropTypes.array.isRequired
 }
 
 export default Table
