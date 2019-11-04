f723e00edf12d00a9a879cffa394b02452c8712e
Konstantin Kirilin <konstantin@paulcamper.de>
2019-11-04 16:02:54 +0300

    added more styles to commentaries component

--- src/components/Commentaries/Commentaries.js
+++ src/components/Commentaries/Commentaries.js
@@ -8,20 +8,29 @@ const Commentaries = (props) => {
     return (
         <div className='commentaries'>
             <div className='comments'>
-                <p>Комментарии:</p>
+                <p className='comments__title'>Комментарии:</p>
                 <div className='comments__list'>
                     <div className='comment'>
                         <div className='comment__author'>
                             <PersonItem />
                         </div>
-                        <div className='comment__message'></div>
+                        <div className='comment__info'>
+                            <div className='comment__time'>09.08.2019. 16:52</div>
+                            <div className='comment__message'>
+                                Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
+                                Aliquid animi harum id mollitia possimus soluta tempore?
+                                Cumque cupiditate deserunt expedita incidunt molestias nesciunt
+                                nihil nisi, officia quas quod sit voluptatibus.
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className='message'>
-                <p>Комментарии:</p>
+                <p className='comments__title'>Написать сообщение:</p>
                 <div className='message-box'>
-                    <textarea />
+                    <textarea className='message-box__input' />
                     <div className='message-box__bottom'>
 
                     </div>
