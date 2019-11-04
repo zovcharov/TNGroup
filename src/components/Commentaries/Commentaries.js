5a54e3e4cafcbee5dc861e4b9077d46a48421ec2
Konstantin Kirilin <konstantin@paulcamper.de>
2019-11-04 16:06:50 +0300

    fixed indents

--- src/components/Commentaries/Commentaries.js
+++ src/components/Commentaries/Commentaries.js
@@ -1,47 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import PersonItem from "../PersonItem/PersonItem";
+import React from 'react';
+import PropTypes from 'prop-types';
+import PersonItem from '../PersonItem/PersonItem';
 
-import './Commentaries.scss'
+import './Commentaries.scss';
 
-const Commentaries = (props) => {
-    return (
-        <div className='commentaries'>
-            <div className='comments'>
-                <p className='comments__title'>Комментарии:</p>
-                <div className='comments__list'>
-                    <div className='comment'>
-                        <div className='comment__author'>
-                            <PersonItem />
-                        </div>
-                        <div className='comment__info'>
-                            <div className='comment__time'>09.08.2019. 16:52</div>
-                            <div className='comment__message'>
+const Commentaries = (props) => (
+    <div className="commentaries">
+        <div className="comments">
+            <p className="comments__title">Комментарии:</p>
+            <div className="comments__list">
+                <div className="comment">
+                    <div className="comment__author">
+                        <PersonItem />
+                    </div>
+                    <div className="comment__info">
+                        <div className="comment__time">09.08.2019. 16:52</div>
+                        <div className="comment__message">
                                 Lorem ipsum dolor sit amet,
                                 consectetur adipisicing elit.
                                 Aliquid animi harum id mollitia possimus soluta tempore?
                                 Cumque cupiditate deserunt expedita incidunt molestias nesciunt
                                 nihil nisi, officia quas quod sit voluptatibus.
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='message'>
-                <p className='comments__title'>Написать сообщение:</p>
-                <div className='message-box'>
-                    <textarea className='message-box__input' />
-                    <div className='message-box__bottom'>
-
-                    </div>
-                </div>
+        </div>
+        <div className="message">
+            <p className="comments__title">Написать сообщение:</p>
+            <div className="message-box">
+                <textarea className="message-box__input" />
+                <div className="message-box__bottom" />
             </div>
         </div>
-    )
-}
+    </div>
+);
 
 Commentaries.propTypes = {
 
-}
+};
 
-export default Commentaries
+export default Commentaries;
