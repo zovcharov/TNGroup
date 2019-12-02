e5df0a0ff588686d912bb45eff659eb55db4c3e3
Konstantin Kirilin <konstantin@paulcamper.de>
2019-12-02 12:32:33 +0300

    Доделал страницу документов

--- src/pages/UserDocumentsPage/UserDocumentsPage.js
+++ src/pages/UserDocumentsPage/UserDocumentsPage.js
@@ -3,9 +3,57 @@ import { connect } from 'react-redux';
 import Table from "../../components/Table/Table";
 import {userDocumentsFetch, userDocumentsUpdate} from "../../redux/actions";
 import {fetchUserDocuments} from "../../redux/fetchers";
+import Container from "../../components/Container/Container";
+import './UserDocumentsPage.scss';
 
 const USER_DOCUMENTS_PAGE_COLUMNS = [
+    {
+        label: 'Название',
+        name: 'namedLink',
+        width: '30%',
+        cell: (item) => <a className='document__name' href={item.link}>{item.name}</a>
+    },
+    {
+        label: 'Автор',
+        name: 'Author',
+        width: '30%',
+    },
+    {
+        label: 'Описание',
+        name: 'Description',
+        width: '40%',
+    }
+]
 
+export const prepareUserDocuments = (data) => {
+    return data.map(item => ({
+        ...item,
+        namedLink: {
+            name: item.Name,
+            link: item.LocalPath,
+        },
+    }))
+};
+
+const documentsMock = [
+    {
+        Name: 'Первый док',
+        LocalPath: '#',
+        Description: 'qweqweqweqweqwe',
+        Author: 'Петров Петр Петрович'
+    },
+    {
+        Name: 'Второй док',
+        LocalPath: '#',
+        Description: 'qweqweqweqweqwe',
+        Author: 'Петров Петр Петрович'
+    },
+    {
+        Name: 'Третий док',
+        LocalPath: '#',
+        Description: 'qweqweqweqweqwe',
+        Author: 'Петров Петр Петрович'
+    },
 ]
 
 const UserDocumentsPage = (props) => {
@@ -23,7 +71,9 @@ const UserDocumentsPage = (props) => {
     })
     return (
         <div>
-            {/*<Table columns={} items={}/>*/}
+            <Container label='Документы:'>
+                <Table columns={USER_DOCUMENTS_PAGE_COLUMNS} items={prepareUserDocuments(documentsMock)}/>
+            </Container>
         </div>
     );
 }
