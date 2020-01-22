e9ecb044fcb500c5dfe8f7652950091aef323bb9
Zakhar Ovcharov <42178043+zovcharov@users.noreply.github.com>
2020-01-22 22:08:45 +0300

    Files for task (#41)
    * Uploading and deleting files in Task modal
    Files view on Task info page
    
    * Code review changes
    
    * Code review changes

--- src/components/ProjectFiles/ProjectFiles.js
+++ src/components/ProjectFiles/ProjectFiles.js
@@ -6,35 +6,15 @@ import React from 'react';
 import './ProjectFiles.scss';
 import DefaultButton from '../Buttons/DefaultButton/DefaultButton';
 
-const FILES = [
-    {
-        name: 'Раз',
-        link: '',
-    },
-    {
-        name: 'Два',
-        link: '',
-    },
-    {
-        name: 'Три',
-        link: '',
-    },
-    {
-        name: 'Чотыри',
-        link: '',
-    },
-];
-
-const ProjectFiles = () => (
+const ProjectFiles = ({ files }) => (
     <div className="project-files">
         <div className="project-files__header">
             <p>Файлы проекта</p>
         </div>
         <div className="project-files__content">
-            {FILES.map(({ name, link }, index) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <div className="project-files__item" key={index}>
-                    <a className="project-files__link" href={link}>{name}</a>
+            {files.map(({ Name, Id }) => (
+                <div className="project-files__item" key={Id}>
+                    <a className="project-files__link">{Name}</a>
                 </div>
             ))}
         </div>
